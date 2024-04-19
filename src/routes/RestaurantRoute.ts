d61af45d92b80cd1cf51661b37f53f6bfb50b134
Dominik Perlak <dominikperlak2@gmail.com>
2024-04-19 11:28:51 +0200

    adding checkout flow

--- src/routes/RestaurantRoute.ts
+++ src/routes/RestaurantRoute.ts
@@ -3,6 +3,14 @@ import { param } from "express-validator";
 import RestaurantController from "../controllers/RestaurantController";
 
 const router = express.Router();
+router.get("/:restaurantId",
+ param("restaurantId")
+.isString()
+.trim()
+.notEmpty()
+.withMessage("City paramenter must be a valid string"), 
+RestaurantController.getRestaurant
+);
 
 router.get(
   "/search/:city",
